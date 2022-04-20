45fad8dccd072f1b136cf72b87860587e60e547c
Susan0223 <sl6994@nyu.edu>
2022-04-20 13:44:25 -0400

    finished

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
+const { Socket } = require('engine.io');
 const express = require('express');
 const app = express();
-
-// we want to add socket.io to our express app
-
-// 1. create a regular http server from our app
 const server = require('http').Server(app);
-
-// 2. mount socket.io (websocket) server on top of that
-io = require('socket.io')(server)
-
-// client portion goes here
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
-let count = 0;
+const io = require('socket.io')(server);
+app.use(express.static('public'));
+// server code goes here!
+// first listen for connection using io.on
+// then... within callback, use socket.on, socket.emit, socket.broadcast, etc.
+// NOTE THAT WE ARE LISTENING WITH server, NOT app!
 
 io.on('connect', socket => {
-  // socket reprsents connection w/ specific client
-  // socket.id is unique identifier for connected client
-  // everytime client connects log it out
+    // socket reprsents connection w/ specific client
+    // socket.id is unique identifier for connected client
+    // everytime client connects log it out
+    
+    console.log(socket.id, 'has just connected');
+    // we want to handle any chat message sent to us from a client
+    socket.on('click1', data => {
+      console.log('got message', data); 
+      // send to everyone
+      io.emit('click1', data);
+
+    });
+    socket.on('click2', data => {
+        console.log('got message', data); 
+        // send to everyone
+        io.emit('click2', data);
   
-  console.log(socket.id, 'has just connected');
-  // we want to handle any chat message sent to us from a client
-  socket.on('chat', data => {
-    console.log('got message', data); 
-    // send to everyone
-    io.emit('chat', data);
+      });
   });
-  socket.on('movement', data => {
-    const {x, y} = data
-    const newData = {x, y, socketID: socket.id};
-    socket.broadcast.emit('movement', newData);
-  });
-});
 
-/*
-function sendCount() {
-  // all connected clients (don't have to use socket)
-  // (don't have to be in callback for this)
-  // send event w/ data to all clients
-  console.log(count);
-  io.emit('count', {count});
-  count += 1;
-}
-setInterval(sendCount, 1000);
-*/
 
+  
 server.listen(3000);
--- public/racer.js
+++ public/racer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  const socket = io();
+  const sock = io();
   const button1 = document.getElementsByClassName('player1Btn')[0];
   const button2 = document.getElementsByClassName('player2Btn')[0];
 
@@ -16,6 +16,7 @@ function main() {
     player1Position += 20;
     player1.style.left = player1Position + 'px';
     
+    sock.emit("click1", {x: player1Position})
     event.preventDefault();
   })
 
@@ -28,6 +29,24 @@ function main() {
     player2.style.left = player2Position + 'px';
 
     event.preventDefault();
+    sock.emit("click2", {x: player2Position})
+    event.preventDefault();
   });
+  sock.on("click1",function(data){
+    const player1 = document.getElementsByClassName('racer player1')[0];
+    const player2 = document.getElementsByClassName('racer player2')[0];
+    player1.style.position = 'relative';
+    player2.style.position = 'relative';
+    
+    player1.style.left = data.x + 'px';
+  })
+  sock.on("click2",function(data){
+    const player1 = document.getElementsByClassName('racer player1')[0];
+    const player2 = document.getElementsByClassName('racer player2')[0];
+    player1.style.position = 'relative';
+    player2.style.position = 'relative';
+    player2Position += 20;
+    player2.style.left = data.x + 'px';
+  })
 
 }
